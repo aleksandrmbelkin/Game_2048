ff63a459df0e3b1cdeb61df7d728c821c39477a7
Александр <74454696+aleksandrmbelkin@users.noreply.github.com>
2024-11-09 22:23:19 +0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import csv
+import random
+from PyQt6 import QtCore, QtWidgets, QtGui
+from PyQt6.QtWidgets import QMainWindow, QApplication
+
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    # Итнерфейс из QtDesigner
+    def setupUi(self, Main):
+        pass
