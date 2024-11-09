096f07cb9929c6d28bc07631f2c5049e5d3bee32
Александр <74454696+aleksandrmbelkin@users.noreply.github.com>
2024-11-09 22:23:32 +0300

    Update game.py

--- src/game.py
+++ src/game.py
@@ -1,1 +1,14 @@
-print('test')
+import csv
+import random
+from PyQt6 import QtCore, QtWidgets, QtGui
+from PyQt6.QtWidgets import QMainWindow, QApplication
+
+
+class Game(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    # Итнерфейс из QtDesigner
+    def setupUi(self, Game):
+        pass
